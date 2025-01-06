31e1dbd95f0b78a05a6d33d244470a914287aada
Marvinx9 <afraniodantas0224@gmail.com>
2025-01-06 09:55:07 -0300

    feat: add user entitie and resource

--- src/main/java/com/educandoweb/EducandowebApplication.java
+++ src/main/java/com/educandoweb/EducandowebApplication.java
@@ -1,4 +1,4 @@
-package com.educandoweb.educandoweb;
+package com.educandoweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
