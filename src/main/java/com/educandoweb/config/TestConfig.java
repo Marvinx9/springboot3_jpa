e01832ba1ab194da188cc7b3ee9cf3b185499902
Afranio Nunes Dantas <afraniodantas0224@gmail.com>
2025-01-12 21:51:42 -0300

    feat: increment enum on order status

--- src/main/java/com/educandoweb/config/TestConfig.java
+++ src/main/java/com/educandoweb/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.entities.Order;
 import com.educandoweb.entities.User;
+import com.educandoweb.enums.OrderStatus;
 import com.educandoweb.repositories.OrderRepository;
 import com.educandoweb.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
     User u1 = new User(null, "Billy Brown", "billy@gmail.com", "77777777", "12345");
     User u2 = new User(null, "Snowbird Brown", "snowbird@gmail.com", "88888888", "12345");
  
-    Order o1 = new Order(null, Instant.parse("2025-01-12T19:44:41Z"), u1);
-    Order o2 = new Order(null, Instant.parse("2025-01-01T08:10:00Z"), u2);
-    Order o3 = new Order(null, Instant.parse("2025-01-10T22:02:10Z"), u1);
+    Order o1 = new Order(null, Instant.parse("2025-01-12T19:44:41Z"), OrderStatus.PAID, u1);
+    Order o2 = new Order(null, Instant.parse("2025-01-01T08:10:00Z"), OrderStatus.WAITNG_PAYMENT, u2);
+    Order o3 = new Order(null, Instant.parse("2025-01-10T22:02:10Z"), OrderStatus.WAITNG_PAYMENT, u1);
 
     userRepository.saveAll(Arrays.asList(u1, u2));
     orderRepository.saveAll(Arrays.asList(o1, o2, o3));
